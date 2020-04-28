7456c161a0ad886af985f8a5a246687d60b15720
saranonearth <saran.suresh@outlook.com>
2020-04-28 21:49:39 +0530

    Setup

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface IProps {}
+
+const App: React.FunctionComponent<IProps> = () => {
+  return (
+    <div>
+      <h1>hey</h1>
+    </div>
+  );
+};
+
+export default App;
